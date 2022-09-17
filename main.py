edd151e8fa9d1641dd803d03c124b7e893cd2961
eyrichj <37912323+eyrichj@users.noreply.github.com>
2022-09-16 21:53:21 -0400

    added current location and openweathermap

---
+++ main.py
@@ -0,0 +1,24 @@
+# Fuck the weather is a python program that
+# scrapes data from the openweathermap api using your current latitude and longitude
+# via geocoder and presents the weather in a cold and salty manor.
+# Developed by Joe Gossett
+
+# Imports
+import requests, json
+import geocoder
+
+# geocoder
+g = geocoder.ip('me')
+lat = g.lat
+lon = g.lng
+
+#debug
+print(lat, lon)
+
+API_KEY = 'b2223f1470ff11a36f948d1bc8e037d1'
+
+url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}'
+
+response = requests.get(url).json()
+
+print(response)
